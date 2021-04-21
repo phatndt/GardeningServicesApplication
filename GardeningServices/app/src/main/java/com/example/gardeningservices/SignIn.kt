d9832a8ef3df1f41f606927556f122a97b601635
nhatkb2001 <62232937+nhatkb2001@users.noreply.github.com>
2021-04-21 09:46:55 +0700

    fix rename files and icons

--- GardeningServices/app/src/main/java/com/example/gardeningservices/SignIn.kt
+++ GardeningServices/app/src/main/java/com/example/gardeningservices/SignIn.kt
@@ -1,23 +1,22 @@
 package com.example.gardeningservices
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-        val tvRegister :TextView=findViewById(R.id.tvregister)
+        val tvRegister :TextView=findViewById(R.id.tv_register)
         tvRegister.setOnClickListener {
-            val intent=Intent(this,Register::class.java);
+            val intent=Intent(this, Register::class.java);
             startActivity(intent)
         }
-        val edMail :EditText=findViewById(R.id.edEmail)
+        val edMail :EditText=findViewById(R.id.edt_email)
+
     }
 }
