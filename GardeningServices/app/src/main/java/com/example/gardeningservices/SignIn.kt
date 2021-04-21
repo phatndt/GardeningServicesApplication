dc5b70207c29ee807ccec1fed2b9e58d65094669
nhatkb2001 <62232937+nhatkb2001@users.noreply.github.com>
2021-04-21 10:52:53 +0700

    Update SignIn.kt

--- GardeningServices/app/src/main/java/com/example/gardeningservices/SignIn.kt
+++ GardeningServices/app/src/main/java/com/example/gardeningservices/SignIn.kt
@@ -2,8 +2,10 @@ package com.example.gardeningservices
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
@@ -11,13 +13,13 @@ class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-        val tvRegister :TextView=findViewById(R.id.tv_register)
-        tvRegister.setOnClickListener {
+        val tv_register :TextView=findViewById(R.id.tv_register)
+        tv_register.setOnClickListener {
             val intent=Intent(this, Register::class.java);
             startActivity(intent)
         }
-        val edMail :EditText=findViewById(R.id.edt_email)
-        btSignIn.setOnClickListener {
+        val button_sign_in:Button=findViewById(R.id.button_sign_in)
+        button_sign_in.setOnClickListener {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             Toast.makeText(this,"Sign In", Toast.LENGTH_LONG).show()
