b329877ec351f3c088d3ec0b23d39782c470284c
nhatkb2001 <62232937+nhatkb2001@users.noreply.github.com>
2021-04-28 14:18:23 +0700

    fix name + icons color

--- GardeningServices/app/src/main/java/com/example/gardeningservices/Register Activity.kt
+++ GardeningServices/app/src/main/java/com/example/gardeningservices/Register Activity.kt
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
-class Register : AppCompatActivity() {
+class `Register Activity` : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         val tvSignIn:TextView=findViewById(R.id.tv_sign_in)
         tvSignIn.setOnClickListener {
-            val intent=Intent(this,SignIn::class.java)
+            val intent=Intent(this,`Sign In Activity`::class.java)
             startActivity(intent);
         }
     }
