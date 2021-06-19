42f5f4456d3a862dff4f12d6790e35d745fc34b5
thanhphat219 <phat01283249260@gmail.com>
2021-06-19 15:52:52 +0700

    update php

--- serverTest/getProductListById.php
+++ serverTest/getProductListById.php
@@ -1,29 +1,23 @@
 <?php
- 
-    /*
-    * Following code will list all the products
-    */
-    // array for JSON response
+
     $response = array();
     
-    // include db connect class
     require_once __DIR__ . '/db_connect.php';
     
-    // connecting to db
     $db = new db_connect();
 
-    $idCategory = $_POST['id'];
-    // get all products from products table
-    $result = mysqli_query($db->connect(),"SELECT * FROM product WHERE idCategory = '$idCategory'") or die(mysqli_error());
+    $idProductList = $_POST['idProductList'];
+    $idProductList = "";
+
+    foreach ($idProductList as $idProduct) {
+        $result = mysqli_query($db->connect(),"SELECT * FROM product WHERE idProduct = $idProduct") or die(mysqli_error());
     
-    // check for empty result
     if (mysqli_num_rows($result) > 0) {
-        // looping through all results
-        // products node
+
         $response = array();
     
         while ($row = mysqli_fetch_array($result)) {
-            // temp user array
+
             $product = array();
             $product["id"] = $row["id"];
             $product["idCategory"] = $row["idCategory"];
@@ -34,20 +28,15 @@
             $product["quantity"]=$row["quantity"];
             $product["rating"]=$row["rating"];
             $product["note"]=$row["note"];
-            // push single product into final response array
+
             array_push($response, $product);
         }
-        // success
-        // $response["success"] = 1;
-    
-        // echoing JSON response
+
         echo json_encode($response);
+
     } else {
-        // no products found
-        // $response["success"] = 0;
-        // $response["message"] = "No products found";
-    
-        // echo no users JSON
+
         echo json_encode($response);
+
     }
 ?>
